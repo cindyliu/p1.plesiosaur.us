6539aa33472ced98c97dcb50f7dfb958a074ab60
Cindy Liu <womp@womp.womp>
2013-09-19 22:59:53 -0400

    completed day or night project

--- daynight.php
+++ daynight.php
@@ -3,10 +3,28 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Day or Night?</title>
-
+		<link rel="stylesheet" href="dn_style.css">
+		<?php require_once('dnlogic.php'); ?>
 	</head>
 
 	<body>
-		Day or Night?
+	        <style type='text/css'>
+		        body {
+			        background-color: <?=$bg_color?>;
+			        color: <?=$text_color?>;
+			        text-align: center;
+			}
+			div.container {
+			        height: 3em;
+			        overflow: auto;
+			        margin: auto;
+			        position: absolute;
+			        top: 0; right: 0; bottom: 0; left: 0;
+			}
+		</style>
+		
+		<div class="container">
+		        It is currently <?=$day_or_night?>time.
+		</div>
 	</body>
 </html>
---
+++ dnlogic.php
@@ -0,0 +1,17 @@
+<?php
+
+$hour = date('G');
+
+if($hour >= 6 AND $hour <= 20) {
+	 $day_or_night = "day";
+	 $bg_color = "#4cf";
+	 $text_color = "black";
+}
+else {
+	 $day_or_night = "night";
+	 $bg_color = "#113";
+	 $text_color = "white";
+}
+
+
+?>
