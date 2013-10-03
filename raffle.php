6016b9c7b5ce5297ed377046e7043c5ef60aab54
Cindy Liu <womp@womp.womp>
2013-10-02 23:45:35 -0400

    forgot to send form submit to raffle.php instead of demo.php; also changed button to say play again instead of pick a winner every time

--- raffle.php
+++ raffle.php
@@ -9,21 +9,22 @@
 
 	<body>
 
-	<form method='POST' action='demo.php'>
+	<form method='POST' action='raffle.php'>
 		Enter 5 contestants' names or email addresses.<br>
 		If you enter an email address and it wins, an email will be sent to that address informing the owner that s/he has won.<br>
 		<?php if(count($_POST) != 0): ?>
-			<?php foreach($_POST as $k => $v): ?>
-				<input type='text' name='<?=$k?>' value='<?=$v?>'><br>
-			<?php endforeach; ?>
-		<?php else: ?>
 			<input type='text' name='contestant1'><br>
 			<input type='text' name='contestant2'><br>
 			<input type='text' name='contestant3'><br>
 			<input type='text' name='contestant4'><br>
 			<input type='text' name='contestant5'><br>
+			<input type='submit' value='Pick a winner!'><br>
+		<?php else: ?>
+			<?php foreach($_POST as $k => $v): ?>
+				<input type='text' name='<?=$k?>' value='<?=$v?>'><br>
+			<?php endforeach; ?>
+			<input type='submit' value='Play again!'><br>
 		<?php endif; ?>
-		<input type='submit' value='Pick a winner!'><br>
 	</form><br>
 
 	<?php if(count($_POST) != 0): ?>
